7e4c45740a07d097042ee2ca157187db1c8d0c81
Gokul D <mgokuldeepak@gmail.com>
2023-09-09 14:09:04 +0530

    Added terraform code for face blur model service from ECR image

---
+++ terraform/modules/ecs_task_definition/inputs.tf
@@ -0,0 +1,4 @@
+variable "public_subnet_a_id" {}
+variable "public_subnet_b_id" {}
+variable "face_blur_model_service_security_group_id" {}
+variable "face_blur_model_service_task_container_name" {}
