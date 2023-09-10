f135250e93022cf833d946a8886ca601ff019241
Gokul D <mgokuldeepak@gmail.com>
2023-09-10 14:53:42 +0530

    Temporarily disabled ALB deletion protection for ease of development.

--- terraform/modules/alb/alb.tf
+++ terraform/modules/alb/alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "privfacy_lb" {
   name                       = "privfacy-lb"
   load_balancer_type         = "application"
   internal                   = false
-  enable_deletion_protection = true
+  enable_deletion_protection = false
   drop_invalid_header_fields = true
 
   security_groups = [var.face_blur_model_service_security_group_id]
