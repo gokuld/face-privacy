83d70dbe3b85f5d0363661bf8ba335b0df73108b
Gokul D <mgokuldeepak@gmail.com>
2023-09-10 18:39:32 +0530

    Added terraform code for Prometheus.

---
+++ terraform/modules/security_groups/prometheus.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "prometheus_sg" {
+  name        = "prometheus-security-group"
+  description = "Security group for the Prometheus EC2 instance."
+
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "Allow incoming traffic on Prometheus default port 9090."
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic."
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
