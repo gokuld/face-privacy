ef5852546eef17c70e2b3e13feb39e6849d536a2
Gokul D <mgokuldeepak@gmail.com>
2023-09-10 15:07:17 +0530

    Added terraform code for face blur Streamlit UI from ECR image

---
+++ terraform/inputs.tf
@@ -0,0 +1,2 @@
+variable "face_blur_model_service_task_container_name" {}
+variable "face_blur_ui_task_container_name" {}
--- terraform/modules/ecs_service/inputs.tf
+++ terraform/modules/ecs_service/inputs.tf
@@ -1,6 +1,12 @@
-variable "face_blur_model_service_task_arn" {}
 variable "public_subnet_a_id" {}
 variable "public_subnet_b_id" {}
+
 variable "face_blur_model_service_security_group_id" {}
 variable "face_blur_model_service_lb_target_group_arn" {}
 variable "face_blur_model_service_task_container_name" {}
+variable "face_blur_model_service_task_arn" {}
+
+variable "face_blur_ui_security_group_id" {}
+variable "face_blur_ui_lb_target_group_arn" {}
+variable "face_blur_ui_task_container_name" {}
+variable "face_blur_ui_task_arn" {}
