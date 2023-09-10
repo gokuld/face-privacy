532583af00a99a3235a9d2aa6a472b82ed1684db
Gokul D <mgokuldeepak@gmail.com>
2023-09-10 15:07:17 +0530

    Added terraform code for face blur Streamlit UI from ECR image

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,7 +1,11 @@
+output "privfacy_alb" {
+  value = aws_lb.privfacy_lb
+}
+
 output "face_blur_model_service_lb_target_group_arn" {
   value = aws_lb_target_group.face_blur_model_service_tg.arn
 }
 
-output "privfacy_alb" {
-  value = aws_lb.privfacy_lb
+output "face_blur_ui_lb_target_group_arn" {
+  value = aws_lb_target_group.face_blur_ui_tg.arn
 }
---
+++ terraform/modules/security_groups/face-blur-ui.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "face_blur_ui_sg" {
+  name        = "face-blur-ui-security-group"
+  description = "Security group for the face blur UI ECS instances and load balancer."
+
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "Allow incoming traffic on port 80."
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic."
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
