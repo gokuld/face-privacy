23c59f9a81cac16c8ff850acb5a0065603e54751
Gokul D <mgokuldeepak@gmail.com>
2023-11-30 13:16:52 +0530

    Prometheus: security updates as suggested by Trivy.
    * Encrypted root block
    * Use HTTP tokens for instance metadata

--- terraform/modules/ec2/prometheus.tf
+++ terraform/modules/ec2/prometheus.tf
@@ -6,6 +6,13 @@ resource "aws_instance" "prometheus_instance" {
   subnet_id     = var.public_subnet_a_id
   ami           = "ami-0ded8326293d3201b" # TODO: make this a tf var
   instance_type = "t2.micro"              # TODO: make this a tf var
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name = "prometheus-instance"
