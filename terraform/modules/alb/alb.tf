5934eef140ec815c4631a9f3fd62596a53d9d5df
Gokul D <mgokuldeepak@gmail.com>
2023-11-30 12:24:01 +0530

    Fixed terraform formatting.

--- terraform/modules/alb/alb.tf
+++ terraform/modules/alb/alb.tf
@@ -7,5 +7,6 @@ resource "aws_lb" "privfacy_lb" {
   drop_invalid_header_fields = true
 
   security_groups = [var.face_blur_model_service_security_group_id, var.face_blur_ui_security_group_id, var.grafana_security_group_id]
-  subnets         = [var.public_subnet_a_id, var.public_subnet_b_id]
+
+  subnets = [var.public_subnet_a_id, var.public_subnet_b_id]
 }
